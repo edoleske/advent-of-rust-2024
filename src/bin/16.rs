8cad4003c6bbd25c2ad8eff7f75a7897f77df08e
Ethan O'Leske <ethan.oleske@gmail.com>
2024-12-21 23:19:29 -0600

    day 16

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -1,3 +1,4 @@
+use std::cmp::PartialEq;
 use std::collections::{HashMap, HashSet, VecDeque};
 
 advent_of_code::solution!(16);
@@ -57,54 +58,104 @@ fn get_path_cost(from: &Index, to: &Index) -> u32 {
     result
 }
 
-fn dijkstra_distance(maze: &Maze) -> Option<u32> {
+fn dijkstra_distance(
+    maze: &Maze,
+    starting_segments: Vec<(Index, Index)>,
+    to: Index,
+) -> HashMap<(Index, Index), u32> {
     // Distances depend on the last step in the path,
     // so this hashmap is mapping nodes to distances and previous nodes
     let mut distances: HashMap<(Index, Index), u32> = HashMap::new();
-    let mut queue: VecDeque<Vec<Index>> = VecDeque::new();
+    let mut queue: VecDeque<(Index, Index, Index)> = VecDeque::new();
 
     // Starting state
-    let direction = Index(maze.start.0 - 1, maze.start.1);
-    distances.insert((direction, maze.start), 0);
-    queue.extend(
-        maze.get_adjacent(&maze.start, &direction)
-            .iter()
-            .map(|&n| vec![direction, maze.start, n]),
-    );
+    distances.extend(starting_segments.iter().map(|&k| (k, 0)));
+    for segment in starting_segments {
+        queue.extend(
+            maze.get_adjacent(&segment.1, &segment.0)
+                .iter()
+                .map(|&n| (segment.0, segment.1, n)),
+        );
+    }
 
-    while let Some(v) = queue.pop_front() {
-        let (last, node, next) = (v[v.len() - 3], v[v.len() - 2], v[v.len() - 1]);
-
+    while let Some((last, node, next)) = queue.pop_front() {
         let cost = distances.get(&(last, node)).unwrap() + get_path_cost(&last, &next);
-        if !distances.contains_key(&(node, next)) {
-            distances.insert((node, next), cost);
-        } else if cost < distances[&(node, next)] {
-            distances.insert((node, next), cost);
-        } else {
-            continue;
+        match distances.get(&(node, next)) {
+            Some(d) => {
+                if cost < *d {
+                    distances.insert((node, next), cost);
+                } else {
+                    continue;
+                }
+            }
+            None => {
+                distances.insert((node, next), cost);
+            }
         }
 
-        if next == maze.end {
+        if next == to {
             continue;
         }
 
         queue.extend(
             maze.get_adjacent(&next, &node)
                 .iter()
-                .map(|&n| v.iter().chain([n].iter()).cloned().collect()),
+                .map(|&n| (node, next, n)),
         );
     }
 
-    distances.iter().filter(|((_, e), _)| e == &maze.end).map(|(_, &v)| v).min()
+    distances
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let maze = Maze::new(input);
-    dijkstra_distance(&maze)
+    let from = Index(maze.start.0 - 1, maze.start.1);
+    dijkstra_distance(&maze, vec![(from, maze.start)], maze.end)
+        .iter()
+        .filter(|((_, e), _)| e == &maze.end)
+        .map(|(_, &v)| v)
+        .min()
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let maze = Maze::new(input);
+    let from = Index(maze.start.0 - 1, maze.start.1);
+    let start_distances = dijkstra_distance(&maze, vec![(from, maze.start)], maze.end);
+
+    // Walk backwards
+    let segments = vec![
+        (Index(maze.end.0 - 1, maze.end.1), Index(maze.end.0 + 1, maze.end.1)),
+        (Index(maze.end.0 + 1, maze.end.1), Index(maze.end.0 - 1, maze.end.1)),
+        (Index(maze.end.0, maze.end.1 - 1), Index(maze.end.0, maze.end.1 + 1)),
+        (Index(maze.end.0, maze.end.1 + 1), Index(maze.end.0, maze.end.1 - 1)),
+    ]
+    .iter()
+    .filter(|&(s, _)| !maze.walls.contains(s))
+    .map(|&(_, from)| (from, maze.end))
+    .collect();
+    let end_distances = dijkstra_distance(&maze, segments, maze.start);
+
+    let min_cost = start_distances
+        .iter()
+        .filter(|((_, e), _)| e == &maze.end)
+        .map(|(_, &v)| v)
+        .min()
+        .unwrap_or(0);
+    let mut result_set: HashSet<Index> = HashSet::from([maze.end]);
+
+    for ((from, to), cost) in start_distances {
+        let adjacent = maze.get_adjacent(&to, &from);
+        for adj in adjacent {
+            if let Some(c) = end_distances.get(&(adj, to)) {
+                if c + cost + (get_path_cost(&from, &adj) - 1) <= min_cost {
+                    result_set.insert(to);
+                    break;
+                }
+            }
+        }
+    }
+
+    Some(result_set.iter().count() as u32)
 }
 
 #[cfg(test)]
@@ -120,6 +171,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(45));
     }
 }
