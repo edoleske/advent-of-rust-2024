cc874fe23389855800dbd8b174cd2e439a2ec678
Ethan O'Leske <52076832+edoleske@users.noreply.github.com>
2024-12-31 09:58:15 -0600

    cleanup day 5

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,9 +1,9 @@
-use std::collections::HashMap;
+use std::collections::{HashSet};
 
 advent_of_code::solution!(5);
 
-fn parse_input(input: &str) -> (HashMap<u32, Vec<u32>>, Vec<Vec<u32>>) {
-    let mut rules: HashMap<u32, Vec<u32>> = HashMap::new();
+fn parse_input(input: &str) -> (HashSet<(u32, u32)>, Vec<Vec<u32>>) {
+    let mut rules: HashSet<(u32, u32)> = HashSet::new();
     let mut updates: Vec<Vec<u32>> = Vec::new();
 
     for line in input.lines() {
@@ -12,16 +12,10 @@ fn parse_input(input: &str) -> (HashMap<u32, Vec<u32>>, Vec<Vec<u32>>) {
         }
 
         if line.contains('|') {
-            let nums = line
-                .trim()
-                .split('|')
-                .map(|token| token.parse().unwrap())
-                .collect::<Vec<u32>>();
-            if nums.len() != 2 {
-                panic!("Invalid rule: '{}'", line);
-            }
-
-            rules.entry(nums[0]).or_insert(Vec::new()).push(nums[1]);
+            let (s1, s2) = line.trim().split_once('|').unwrap();
+            let n1 = s1.parse::<u32>().unwrap();
+            let n2 = s2.parse::<u32>().unwrap();
+            rules.insert((n1, n2));
         } else {
             let nums = line
                 .trim()
@@ -35,42 +29,16 @@ fn parse_input(input: &str) -> (HashMap<u32, Vec<u32>>, Vec<Vec<u32>>) {
     (rules, updates)
 }
 
-fn is_update_valid(rules: &HashMap<u32, Vec<u32>>, update: &Vec<u32>) -> bool {
-    let mut valid = true;
+fn get_sorted_update(rules: &HashSet<(u32, u32)>, update: &Vec<u32>) -> Vec<u32> {
+    let mut result = update.clone();
 
-    let l = update.len();
-    'outer: for i in 1..l {
-        let page = &update[l - i];
-        if rules.contains_key(page) {
-            let before_pages = &update[0..l - i];
-            for p in before_pages {
-                if rules[page].contains(p) {
-                    valid = false;
-                    break 'outer;
-                }
-            }
-        }
-    }
-
-    valid
-}
-
-fn fix_update(rules: &HashMap<u32, Vec<u32>>, update: &mut Vec<u32>) {
-    let l = update.len();
-    while !is_update_valid(rules, update) {
-        for i in 1..l {
-            let page = &update[l - i];
-            if rules.contains_key(page) {
-                let before_pages = &update[0..l - i];
-                for p in before_pages {
-                    if rules[page].contains(p) {
-                        update.swap(l - i - 1, l - i);
-                        break;
-                    }
-                }
-            }
-        }
-    }
+    result.sort_by(|a, b| if rules.contains(&(*a, *b)) {
+        std::cmp::Ordering::Less
+    } else {
+        std::cmp::Ordering::Greater
+    });
+    
+    result
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -78,7 +46,8 @@ pub fn part_one(input: &str) -> Option<u32> {
     let (rules, updates) = parse_input(input);
 
     for update in updates {
-        if is_update_valid(&rules, &update) {
+        let sorted = get_sorted_update(&rules, &update);
+        if update.iter().zip(&sorted).all(|(a, b)| a == b) {
             total += update[update.len() / 2];
         }
     }
@@ -90,10 +59,10 @@ pub fn part_two(input: &str) -> Option<u32> {
     let mut total: u32 = 0;
     let (rules, updates) = parse_input(input);
 
-    for mut update in updates {
-        if !is_update_valid(&rules, &update) {
-            fix_update(&rules, &mut update);
-            total += update[update.len() / 2];
+    for update in updates {
+        let sorted = get_sorted_update(&rules, &update);
+        if !update.iter().zip(&sorted).all(|(a, b)| a == b) {
+            total += sorted[sorted.len() / 2];
         }
     }
 
